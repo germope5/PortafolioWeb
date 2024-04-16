539a20db0ba2f7608ab62c511fd42e77d7087a2e
germope5 <dmm71317@gmail.com>
2024-04-15 22:06:30 -0600

    Se agregan botones para contactar al Dev

--- assets/js/app.js
+++ assets/js/app.js
@@ -40,3 +40,21 @@ bioTitle.addEventListener('mouseout', () => {
   bioTitle.style.transform = 'translateY(0)';
 });
 
+var animateButton = function(e) {
+
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove('animate');
+  
+  e.target.classList.add('animate');
+  setTimeout(function(){
+    e.target.classList.remove('animate');
+  },700);
+};
+
+var bubblyButtons = document.getElementsByClassName("bubbly-button");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener('click', animateButton, false);
+}
+
